444b2fff9faefa15ccab7ef2afc84136d6706567
Abhishek-Kuvalekar <abhi.kuvalekar@gmail.com>
2018-03-31 01:00:42 +0530

    Completed Pass two.

--- Hypothetical-Assembler/src/HypotheticalAssembler.py
+++ Hypothetical-Assembler/src/HypotheticalAssembler.py
@@ -24,6 +24,7 @@ errors = {"ORG_ERROR"           : "Invalid operand for ORG.",
           "UNDEFINED_VARIABLE"  : "Undefined variable.",
           "UNUSED_VARIABLE"     : "Unused variable."}
 
+
 class Instruction():
     def __init__(self, opcode, operand, lineNumber):
         self.opcode = opcode
