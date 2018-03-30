35ae2362077e41b5d0e29cc63825f59ffee29cd3
Abhishek-Kuvalekar <abhi.kuvalekar@gmail.com>
2018-03-30 11:53:50 +0530

    Added basic classes for assembler design.

---
+++ Hypothetical-Assembler/src/HypotheticalAssembler.py
@@ -0,0 +1,15 @@
+class Assembler():
+    def __init__(self, inputFile, outputFile):
+        self.inputFile = inputFile
+        self.outputFile = outputFile
+        self.outputDict = dict();
+
+    def parseInputFile(self):
+        pass
+
+    def generateOutputFile(self):
+        pass
+
+class FileManager():
+    def __init__(self, fileName):
+        pass
